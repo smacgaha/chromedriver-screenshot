422d5fb4aee8ee8d57859f26df5002deb0c18b82
Sean MacGahan <smacgaha@gmail.com>
2016-11-28 18:24:45 -0500

    close browser at end of test

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,6 +1,12 @@
+browser = Selenium::WebDriver.for :chrome
+
 Before do
-  @browser = Selenium::WebDriver.for :chrome
+  @browser = browser
 
   # ensure that build/cucumber exists
   FileUtils.mkdir_p logs_directory
+end
+
+at_exit do
+  browser.close
 end
